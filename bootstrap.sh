8ce13806b9240d4bfbcac81d8b1d70d3283848a4
Joshua Spence <josh@joshuaspence.com>
2020-07-19 19:37:21 +1000

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Bootstrap Flux (see https://docs.fluxcd.io/en/1.18.0/tutorials/get-started-helm.html).
+kubectl apply --filename https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/crds.yaml
+kubectl create namespace flux
+helm install --namespace flux --repo https://charts.fluxcd.io --set git.url=https://github.com/joshuaspence/helm-operator-demo.git --set git.readonly=true --set git.path=deployments --wait flux flux >/dev/null
+helm install --namespace flux --repo https://charts.fluxcd.io --set helm.versions=v3 --wait helm-operator helm-operator >/dev/null 
+
+# Force a sync.
+fluxctl --k8s-fwd-ns flux sync
+
+# Check results.
+kubectl -n flux describe helmrelease/helm-operator
